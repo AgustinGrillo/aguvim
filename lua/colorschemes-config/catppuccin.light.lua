020d08eb2876370f6796e935b203dbfd3435e555
Agustin Grillo <grilloagus@gmail.com>
2023-01-05 22:21:30 -0300

    update catppuccin colorscheme config

--- lua/colorschemes-config/catppuccin.light.lua
+++ lua/colorschemes-config/catppuccin.light.lua
@@ -2,7 +2,7 @@ local catppuccin = require("catppuccin")
 
 -- configure it (info at --> https://github.com/catppuccin/nvim)
 catppuccin.setup({
-                  flavour= "mocha",
+                  flavour= "latte",
                  })
 
 vim.cmd.colorscheme "catppuccin"
