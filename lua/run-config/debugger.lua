56088279cf3669a1501191d10885ba71eaa57253
Agustin Grillo <grilloagus@gmail.com>
2025-07-28 14:58:51 -0300

    Debugger: Add venv support

--- lua/run-config/debugger.lua
+++ lua/run-config/debugger.lua
@@ -1,146 +1,159 @@
 local dap = require('dap')
 local dapui = require('dapui')
 
-local home_dir = os.getenv("HOME") 
+local home_dir = os.getenv("HOME")
 
 -- Python Config
 dap.adapters.python = {
-  type = 'executable';
-  command = home_dir ..'/.local/share/nvim/mason/packages/debugpy/venv/bin/python';
-  args = { '-m', 'debugpy.adapter' };
+    type = 'executable',
+    command = home_dir .. '/.local/share/nvim/mason/packages/debugpy/venv/bin/python',
+    args = { '-m', 'debugpy.adapter' },
 }
 
 
 dap.configurations.python = {
-  {
-    -- The first three options are required by nvim-dap
-    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
-    request = 'launch';
-    name = "Launch file";
+    {
+        -- The first three options are required by nvim-dap
+        type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+        request = 'launch',
+        name = "Launch file",
 
-    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+        -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
-    program = "${file}"; -- This configuration will launch the current file if used.
-    pythonPath = function()
-      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-      local conda_path = os.getenv("CONDA_PREFIX") 
-      if (conda_path ~= nil and vim.fn.executable(conda_path .. '/bin/python') == 1) then
-        return conda_path .. '/bin/python'
-      else
-        return '/usr/bin/python3'
-      end
-    end;
-  },
+        program = "${file}", -- This configuration will launch the current file if used.
+        pythonPath = function()
+            -- Look for .venv or venv in the current working directory or its ancestors
+            local cwd = vim.fn.getcwd()
+
+            local paths_to_check = {
+                cwd .. '/.venv/bin/python',
+                cwd .. '/venv/bin/python',
+            }
+
+            for _, path in ipairs(paths_to_check) do
+                if vim.fn.executable(path) == 1 then
+                    return path
+                end
+            end
+
+            -- Look for a conda environment
+            local conda_path = os.getenv("CONDA_PREFIX")
+            if (conda_path ~= nil and vim.fn.executable(conda_path .. '/bin/python') == 1) then
+                return conda_path .. '/bin/python'
+            end
+
+            return '/usr/bin/python3' -- fallback
+        end,
+    },
 }
 
 -- C/C++ Config
 dap.adapters.cppdbg = {
-  id = 'cppdbg',
-  type = 'executable',
-  command = home_dir .. '/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+    id = 'cppdbg',
+    type = 'executable',
+    command = home_dir .. '/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
 }
 
 dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "cppdbg",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = true,
-    setupCommands = {  
-      { 
-         text = '-enable-pretty-printing',
-         description =  'enable pretty printing',
-         ignoreFailures = false 
-      },
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = true,
+        setupCommands = {
+            {
+                text = '-enable-pretty-printing',
+                description = 'enable pretty printing',
+                ignoreFailures = false
+            },
+        },
     },
-  },
-  {
-    name = 'Attach to gdbserver :1234',
-    type = 'cppdbg',
-    request = 'launch',
-    MIMode = 'gdb',
-    miDebuggerServerAddress = 'localhost:1234',
-    miDebuggerPath = '/usr/bin/gdb',
-    cwd = '${workspaceFolder}',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-  },
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+    },
 }
 
 -- UI Config
-vim.fn.sign_define('DapBreakpoint', {text='▶', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpoint', { text = '▶', texthl = '', linehl = '', numhl = '' })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+    dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+    dapui.close()
 end
 
 dapui.setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  controls = {
-    icons = {
-      pause = '⏸',
-      play = '▶',
-      step_into = '⏎',
-      step_over = '⏭',
-      step_out = '⏮',
-      step_back = 'b',
-      run_last = '▶▶',
-      terminate = '⏹',
-      disconnect = '⏏',
-    },
-  },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = vim.fn.has("nvim-0.7"),
-  layouts = {
-    { 
-        elements = {
-            { id = "breakpoints", size = 0.3 },
-            { id = "scopes", size = 0.7 },
+    icons = { expanded = "▾", collapsed = "▸" },
+    controls = {
+        icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
         },
-        size = 40, 
-        position = 'right',
     },
-    {
-        elements = {
-            'repl',
-            'console',
-        },
-        size = 10,
-        position = 'bottom',
-      },
-    },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "rounded", -- Border style. Can be "single", "double" or "rounded"
     mappings = {
-      close = { "q", "<Esc>" },
+        -- Use a table to apply multiple mappings
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
     },
-  },
-  windows = { indent = 1 },
-  render = { 
-    max_type_length = nil, -- Can be integer or nil.
-  }
+    -- Expand lines larger than the window
+    -- Requires >= 0.7
+    expand_lines = vim.fn.has("nvim-0.7"),
+    layouts = {
+        {
+            elements = {
+                { id = "breakpoints", size = 0.3 },
+                { id = "scopes",      size = 0.7 },
+            },
+            size = 40,
+            position = 'right',
+        },
+        {
+            elements = {
+                'repl',
+                'console',
+            },
+            size = 10,
+            position = 'bottom',
+        },
+    },
+    floating = {
+        max_height = nil,   -- These can be integers or a float between 0 and 1.
+        max_width = nil,    -- Floats will be treated as percentage of your screen.
+        border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+            close = { "q", "<Esc>" },
+        },
+    },
+    windows = { indent = 1 },
+    render = {
+        max_type_length = nil, -- Can be integer or nil.
+    }
 })
