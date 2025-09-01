75b5182e5049737ea70892f3e8186cd271ceb018
Agustin Grillo <grilloagus@gmail.com>
2025-09-01 14:46:25 -0300

    Replace orgmode with neorg

--- lua/lazy-config/init.lua
+++ lua/lazy-config/init.lua
@@ -102,7 +102,8 @@ return require('lazy').setup({
     "christoomey/vim-tmux-navigator",
     -- Org
     {
-        'nvim-orgmode/orgmode',
-        event = 'VeryLazy',
+        "nvim-neorg/neorg",
+        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
     }
 })
--- lua/misc/org.lua
+++ lua/misc/org.lua
@@ -1,4 +1,25 @@
-require('orgmode').setup({
-    org_agenda_files = '~/orgfiles/**/*',
-    org_default_notes_file = '~/orgfiles/refile.org',
+require("neorg").setup({
+    load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+    }
+})
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*.norg"},
+  command = "set conceallevel=3",
+})
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*.norg"},
+  command = "set concealcursor=nv"
+})
+
+-- Remap folding keys specifically for Neorg files
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*.norg"},
+  callback = function()
+    vim.keymap.set('n', '<Tab>', 'za', { buffer = true, desc = "Neorg: Toggle fold" })
+    vim.keymap.set('n', '<S-Tab>', 'zA', { buffer = true, desc = "Neorg: Recursively toggle fold" })
+  end,
 })
