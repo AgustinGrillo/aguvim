15c6df0e04c2562af67ca1d003474f5393a1d27f
Agustin Grillo <grilloagus@gmail.com>
2026-08-04 15:52:51 -0300

    Update lazygit keybindings

--- lua/snacks-config/lazygit.lua
+++ lua/snacks-config/lazygit.lua
@@ -2,5 +2,13 @@ return {
     win = {
         width = 0.95,
         height = 0.95,
+        keys = {
+            hide_lazygit = {
+                'q',
+                'hide',
+                mode = 't',
+                desc = 'Hide LazyGit',
+            },
+        },
     },
 }
